d22798972f14323bd8566cf2ff297f1df426e4fc
unknown <dandumitriu33@gmail.com>
2025-11-09 15:00:07 +0200

    Add initial files with Hello World endpoint

---
+++ src/main/java/com/springbootapi/restapi/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.springbootapi.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        // Return a JSON object with "message": "Hello world"
+        return Collections.singletonMap("message", "Hello world");
+    }
+}
