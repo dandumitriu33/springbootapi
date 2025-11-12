f394470d46bfca881008c31fee502eee5e4c0145
unknown <dandumitriu33@gmail.com>
2025-11-12 09:58:20 +0200

    Add database config class

---
+++ src/main/java/com/springbootapi/restapi/config/DatabaseConfig.java
@@ -0,0 +1,42 @@
+package com.springbootapi.restapi.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties properties, HikariConfig hikariConfig) {
+        HikariDataSource ds = new HikariDataSource(hikariConfig);
+        ds.setJdbcUrl(properties.getUrl());
+        ds.setUsername(properties.getUsername());
+        ds.setPassword(properties.getPassword());
+        ds.setDriverClassName(properties.getDriverClassName());
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
+
