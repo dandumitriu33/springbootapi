44417ca1c01d5e295de3b14cc269940f39ae1757
unknown <dandumitriu33@gmail.com>
2025-11-14 18:41:33 +0200

    Add Swagger documentation for endpoints

---
+++ src/main/java/com/springbootapi/restapi/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.springbootapi.restapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Car API educational project")
+                        .description("REST API for managing cars")
+                        .version("1.0.0"));
+    }
+}
+
